f245074d1c826a97db211a106ef23af9f0509266
Matthew <mrw2003x@gmail.com>
2025-03-08 12:12:21 -0800

    connected to remote DB and tested sql create script

--- main.py
+++ main.py
@@ -2,26 +2,39 @@ import psycopg2
 
 # Databse info
 #########################
-hostname = 'localhost'
-database = 'postgres'
-username = 'postgres'
-pwd = 'gocougs25'
-port_id = 5433
+hostname = 'dpg-cv501ql2ng1s73fl3f00-a.oregon-postgres.render.com'
+database = 'gym_membership_application'
+username = 'gym_membership_application_user'
+pwd = '9JDl6xuzyUUZe2mbSoYfTQXxZWllT5IL'
+port_id = 5432
 #########################
 
+conn = None
+cur = None 
+
 # open connection to DB
-conn = psycopg2.connect(
-    host = hostname,
-    dbname = database,
-    user = username,
-    password = pwd,
-    port = port_id
-)
 
-if conn:
-    print("Works")
-else:
-    print("Doesn't Work")
-
-#close connection
-conn.close()
+try:
+    conn = psycopg2.connect(
+        host = hostname,
+        dbname = database,
+        user = username,
+        password = pwd,
+        port = port_id
+    )
+    cur = conn.cursor()
+    test_script = '''
+    CREATE TABLE employee (
+    ID  int primary key,
+    name varchar(40) NOT NULL
+    )
+    '''
+    cur.execute(test_script)
+    conn.commit()          
+except Exception as error:
+    print(error)
+finally:
+        #Close Cursor connection
+    cur.close()
+    #close connection
+    conn.close()
